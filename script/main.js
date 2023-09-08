095880e0cc60e2a49e4c7ae76d4367fc826baae5
Gladys Ganda <gladysganda8@gmail.com>
2023-09-08 23:37:38 +0700

    update errors

--- script/main.js
+++ script/main.js
@@ -7,7 +7,7 @@ window.addEventListener('load', () => {
 // animation timeline
 const animationTimeline = () => {
     // split chars that needs to be animated individually
-    const textBoxChars = document.getElementsByClassName("hbd-chatbox")[0];
+    // const textBoxChars = document.getElementsByClassName("hbd-chatbox")[0];
     const hbd = document.getElementsByClassName("wish-hbd")[0];
 
     // textBoxChars.innerHTML = `<span>${textBoxChars.innerHTML
