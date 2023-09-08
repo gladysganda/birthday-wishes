75a65fb8c3a3a780fa0c2bafd227090c5cff1104
Gladys Ganda <gladysganda8@gmail.com>
2023-09-09 00:00:24 +0700

    error fixes

--- script/main.js
+++ script/main.js
@@ -2,7 +2,6 @@
 window.addEventListener('load', () => {
     Swal.fire({
         title: 'Are you ready?',
-        text: "You won't be able to revert this!",
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
